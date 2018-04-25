f55c4c6b593232e0d6ad71c763d2ca6f2bd5de51
Maël Le Garrec <mlegarrec@online.net>
2018-04-25 15:07:49 +0000

    convert to new build system

---
+++ latest/env.mk
@@ -0,0 +1,7 @@
+IMAGE_NAME =			lemp
+IMAGE_VERSION =		latest
+IMAGE_VERSION_ALIASES =
+IMAGE_TITLE =			LEMP
+IMAGE_DESCRIPTION =		LEMP
+IMAGE_SOURCE_URL =		https://github.com/scaleway-community/scaleway-lemp
+IMAGE_BOOTSCRIPT = mainline 4.9
